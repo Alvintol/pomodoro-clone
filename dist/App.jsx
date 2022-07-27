db0f644d313720e04604c806aa1d14f3130bda5a
Alvin <alvin.tol@hotmail.com>
2022-07-27 15:37:47 -0600

    adds hardcoded componenet placholders

--- dist/App.jsx
+++ dist/App.jsx
@@ -1,6 +1,32 @@
 const App = () => {
-    return (<div className="app">
-
+    return (<div className='app'>
+      <div id='container'>
+        <div id='options'>
+          <div id='session'>Session</div>
+          <div id='short'>
+            <div id='down-short'></div>
+            Short
+            <div id='up-short'></div>
+          </div>
+          <div id='long'>
+            <div id='down-long'></div>
+            Long
+            <div id='up-long'></div>
+          </div>
+          <div id='timer'>
+            Timer
+          </div>
+          <div id='start'>
+            Start
+          </div>
+          <div id='pause'>
+            Pause
+          </div>
+          <div id='reset'>
+            Reset
+          </div>         
+        </div>
+      </div>
     </div>);
 };
 export default App;
