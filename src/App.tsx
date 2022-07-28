53bfef7ce4ef2d8b986f97b95cf643460a9d6e3a
Alvin <alvin.tol@hotmail.com>
2022-07-28 14:54:02 -0600

    adds hover effect to all buttons

--- src/App.tsx
+++ src/App.tsx
@@ -13,22 +13,22 @@ const App = () => {
       >
         <div id='options' className='flex flex-row justify-around text-center'>
           <div id='session'>Session</div>
-          <div id='short' className='flex flex-row'>
-            <i id='down-short' className='fa-solid fa-arrow-down-long'></i>
+          <div id='short' className='flex flex-row items-center'>
+            <i id='down-short' className='fa-solid fa-arrow-down-long mr-1 hover:opacity-50'></i>
             Short
-            <i id='up-short' className='fa-solid fa-arrow-up-long'></i>
+            <i id='up-short' className='fa-solid fa-arrow-up-long ml-1 hover:opacity-50'></i>
           </div>
-          <div id='long' className='flex flex-row'>
-            <i id='down-long' className='fa-solid fa-arrow-down-long'></i>
+          <div id='long' className='flex flex-row items-center'>
+            <i id='down-long' className='fa-solid fa-arrow-down-long mr-1 hover:opacity-50'></i>
             Long
-            <i id='up-long' className='fa-solid fa-arrow-up-long'></i>
+            <i id='up-long' className='fa-solid fa-arrow-up-long ml-1 hover:opacity-50'></i>
           </div>
         </div>
-        <div id='timer' className='h-1/2 text-[50px] sm:text-[75px] md:text-[125px]'>25:00</div>
+        <div id='timer' className='flex flex-col justify-center h-1/2 text-[50px] sm:text-[75px] md:text-[125px]'>25:00</div>
         <div id='controls' className='flex flex-row justify-center'>
-        <i id='start' className="fa-solid fa-play m-1"></i>
-        <i id='pause' className="fa-solid fa-pause m-1"></i>
-        <i id='reset' className="fa-solid fa-rotate m-1"></i>
+        <i id='start' className="fa-solid fa-play m-1 hover:opacity-50"></i>
+        <i id='pause' className="fa-solid fa-pause m-1 hover:opacity-50"></i>
+        <i id='reset' className="fa-solid fa-rotate m-1 hover:opacity-50"></i>
         </div>
       </div>
     </div>
