6e7a838ecd3303ac30409245b894bdef9db9c0f1
Alvin <alvin.tol@hotmail.com>
2022-07-30 15:05:49 -0600

    adds conditional logic to control component for each unique icon

--- src/components/Control.tsx
+++ src/components/Control.tsx
@@ -1,5 +1,19 @@
-const Control = () => {
-  
+const Control = ({ id }: string) => {
+
+  const iconClass = () => {
+    switch (id) {
+      case 'START': return 'fa-solid fa-play mx-2 text-2xl hover:opacity-50';
+      case 'PAUSE': return 'fa-solid fa-pause mx-2 text-2xl hover:opacity-50';
+      case 'RESET': return 'fa-solid fa-reset mx-2 text-2xl hover:opacity-50';
+    }
+  }
+
+  return (
+    <i
+      id='start'
+      className={iconClass()}
+    ></i>
+  );
 };
 
 export default Control;
