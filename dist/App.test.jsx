cdd6e44da8cf84bedba65ca29236d028c615d486
Alvin <alvin.tol@hotmail.com>
2022-07-30 15:59:56 -0600

    adds Session, Break, Control, Control component tests

--- dist/App.test.jsx
+++ dist/App.test.jsx
@@ -17,9 +17,29 @@ describe('Components', () => {
     //   const options = screen.getByTestId('options');
     //   expect(options).toBeInTheDocument();
     // })
+    // it('renders Session component', () => {
+    //   render(<Session />);
+    //   const session = screen.getByTestId('session');
+    //   expect(session).toBeInTheDocument();
+    // })
+    // it('renders Break component', () => {
+    //   render(<Break />);
+    //   const break = screen.getByTestId('break');
+    //   expect(break).toBeInTheDocument();
+    // })
     it('renders Timer component', () => {
         render(<Timer />);
         const timer = screen.getByTestId('timer');
         expect(timer).toBeInTheDocument();
     });
+    // it('renders Controls component', () => {
+    //   render(<Controls />);
+    //   const controls = screen.getByTestId('controls');
+    //   expect(controls).toBeInTheDocument();
+    // })
+    // it('renders Control component', () => {
+    //   render(<Control />);
+    //   const control = screen.getByTestId('control');
+    //   expect(control).toBeInTheDocument();
+    // })
 });
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -23,11 +23,36 @@ describe('Components', () => {
   //   expect(options).toBeInTheDocument();
   // })
   
+  // it('renders Session component', () => {
+  //   render(<Session />);
+  //   const session = screen.getByTestId('session');
+  //   expect(session).toBeInTheDocument();
+  // })
+  
+  // it('renders Break component', () => {
+  //   render(<Break />);
+  //   const break = screen.getByTestId('break');
+  //   expect(break).toBeInTheDocument();
+  // })
+  
   it('renders Timer component', () => {
     render(<Timer />);
     const timer = screen.getByTestId('timer');
     expect(timer).toBeInTheDocument();
   })
+
+  // it('renders Controls component', () => {
+  //   render(<Controls />);
+  //   const controls = screen.getByTestId('controls');
+  //   expect(controls).toBeInTheDocument();
+  // })
+  
+  // it('renders Control component', () => {
+  //   render(<Control />);
+  //   const control = screen.getByTestId('control');
+  //   expect(control).toBeInTheDocument();
+  // })
+  
 
 
 });
