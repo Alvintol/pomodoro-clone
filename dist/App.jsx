bf1744a65c45ad52f0e9907b8cd37c0c1ac32e5d
Alvin <alvin.tol@hotmail.com>
2022-07-30 14:57:16 -0600

    adds control button component

--- dist/App.jsx
+++ dist/App.jsx
@@ -1,6 +1,6 @@
 import './css/main.css';
 const App = () => {
-    return (<div id='app' className='bg-orange text-center h-screen flex flex-col items-center justify-center'>
+    return (<div data-testid='app' id='app' className='bg-orange text-center h-screen flex flex-col items-center justify-center'>
       <div id='container' className='bg-purple flex flex-col justify-around h-1/2 w-5/6 rounded-lg p-3'>
         <div id='options' className='bg-pink flex flex-col items-center text-center rounded-md md:justify-around md:flex-row mx-3'>
           <div id='session'>Session</div>
@@ -15,11 +15,13 @@ const App = () => {
             <i id='up-long' className='fa-solid fa-arrow-up-long ml-1 hover:opacity-50'></i>
           </div>
         </div>
-        <div id='timer' className='flex flex-col justify-center h-1/2 font-bold text-[50px] sm:text-[75px] md:text-[125px]'>25:00</div>
+        <div id='timer' className='flex flex-col justify-center h-1/2 font-bold text-[50px] sm:text-[75px] md:text-[125px]'>
+          25:00
+        </div>
         <div id='controls' className='flex flex-row justify-center'>
-        <i id='start' className="fa-solid fa-play mx-2 text-2xl hover:opacity-50"></i>
-        <i id='pause' className="fa-solid fa-pause mx-2 text-2xl hover:opacity-50"></i>
-        <i id='reset' className="fa-solid fa-rotate mx-2 text-2xl hover:opacity-50"></i>
+          <i id='start' className='fa-solid fa-play mx-2 text-2xl hover:opacity-50'></i>
+          <i id='pause' className='fa-solid fa-pause mx-2 text-2xl hover:opacity-50'></i>
+          <i id='reset' className='fa-solid fa-rotate mx-2 text-2xl hover:opacity-50'></i>
         </div>
       </div>
     </div>);
