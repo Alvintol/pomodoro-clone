4636ed33f33261a7471478688a219230a2f84372
Alvin <alvin.tol@hotmail.com>
2022-07-30 16:15:07 -0600

    imports state into Session component

--- dist/components/Session.jsx
+++ dist/components/Session.jsx
@@ -1,4 +1,6 @@
+import { useAppState } from '../state/StateProvider';
 const Session = () => {
+    const state = useAppState();
     return (<div id='session'>Session</div>);
 };
 export default Session;
--- src/components/Session.tsx
+++ src/components/Session.tsx
@@ -1,4 +1,9 @@
+import { useAppState } from '../state/StateProvider';
+
 const Session = () => {
+
+  const state = useAppState();
+
   return (
     <div id='session'>Session</div>
   )
