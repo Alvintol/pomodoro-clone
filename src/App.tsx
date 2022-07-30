2b77b6ca9503037c0b1d82b4ba259e6cbc763376
Alvin <alvin.tol@hotmail.com>
2022-07-29 22:11:56 -0600

    adds app render test

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './css/main.css';
 const App = () => {
   return (
     <div
+      data-testid='app'
       id='app'
       className='bg-orange text-center h-screen flex flex-col items-center justify-center'
     >
@@ -10,24 +11,59 @@ const App = () => {
         id='container'
         className='bg-purple flex flex-col justify-around h-1/2 w-5/6 rounded-lg p-3'
       >
-        <div id='options' className='bg-pink flex flex-col items-center text-center rounded-md md:justify-around md:flex-row mx-3'>
-          <div id='session' >Session</div>
-          <div id='short' className='flex flex-row w-full justify-between items-center px-5 sm:w-3/4 md:w-1/6'>
-            <i id='down-short' className='fa-solid fa-arrow-down-long mr-1 hover:opacity-50 '></i>
+        <div
+          id='options'
+          className='bg-pink flex flex-col items-center text-center rounded-md md:justify-around md:flex-row mx-3'
+        >
+          <div id='session'>Session</div>
+          <div
+            id='short'
+            className='flex flex-row w-full justify-between items-center px-5 sm:w-3/4 md:w-1/6'
+          >
+            <i
+              id='down-short'
+              className='fa-solid fa-arrow-down-long mr-1 hover:opacity-50 '
+            ></i>
             Short
-            <i id='up-short' className='fa-solid fa-arrow-up-long ml-1 hover:opacity-50'></i>
+            <i
+              id='up-short'
+              className='fa-solid fa-arrow-up-long ml-1 hover:opacity-50'
+            ></i>
           </div>
-          <div id='long' className='flex flex-row w-full justify-between items-center px-5 sm:w-3/4 md:w-1/6'>
-            <i id='down-long' className='fa-solid fa-arrow-down-long mr-1 hover:opacity-50'></i>
+          <div
+            id='long'
+            className='flex flex-row w-full justify-between items-center px-5 sm:w-3/4 md:w-1/6'
+          >
+            <i
+              id='down-long'
+              className='fa-solid fa-arrow-down-long mr-1 hover:opacity-50'
+            ></i>
             Long
-            <i id='up-long' className='fa-solid fa-arrow-up-long ml-1 hover:opacity-50'></i>
+            <i
+              id='up-long'
+              className='fa-solid fa-arrow-up-long ml-1 hover:opacity-50'
+            ></i>
           </div>
         </div>
-        <div id='timer' className='flex flex-col justify-center h-1/2 font-bold text-[50px] sm:text-[75px] md:text-[125px]'>25:00</div>
+        <div
+          id='timer'
+          className='flex flex-col justify-center h-1/2 font-bold text-[50px] sm:text-[75px] md:text-[125px]'
+        >
+          25:00
+        </div>
         <div id='controls' className='flex flex-row justify-center'>
-        <i id='start' className="fa-solid fa-play mx-2 text-2xl hover:opacity-50"></i>
-        <i id='pause' className="fa-solid fa-pause mx-2 text-2xl hover:opacity-50"></i>
-        <i id='reset' className="fa-solid fa-rotate mx-2 text-2xl hover:opacity-50"></i>
+          <i
+            id='start'
+            className='fa-solid fa-play mx-2 text-2xl hover:opacity-50'
+          ></i>
+          <i
+            id='pause'
+            className='fa-solid fa-pause mx-2 text-2xl hover:opacity-50'
+          ></i>
+          <i
+            id='reset'
+            className='fa-solid fa-rotate mx-2 text-2xl hover:opacity-50'
+          ></i>
         </div>
       </div>
     </div>
