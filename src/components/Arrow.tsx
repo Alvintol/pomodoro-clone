f33cf4c83c3193a44c6e5ccab497e917cae28add
Alvin <alvin.tol@hotmail.com>
2022-07-30 16:30:41 -0600

    adds classname conditional rendering and interface for arrow component

--- src/components/Arrow.tsx
+++ src/components/Arrow.tsx
@@ -1,8 +1,19 @@
-const Arrow = () => {
+interface arrowBtn {
+  id: string;
+  type: string;
+  className: string;
+}
+
+const Arrow = ({ id, type }: arrowBtn) => {
+
+  const arrowClass = type === 'UP' ? 'fa-solid fa-arrow-up-long mr-1 hover:opacity-50' : 'fa-solid fa-arrow-down-long mr-1 hover:opacity-50 '
 
   return (
-    
-  )
+    <i
+      id={id}
+      className={arrowClass}
+    ></i>
+  );
 };
 
 export default Arrow;
