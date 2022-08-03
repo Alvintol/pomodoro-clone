6036785ef90af8b58d9a6cf4cb21fa78db3521f8
Alvin <alvin.tol@hotmail.com>
2022-08-02 19:23:39 -0600

    adds option context

--- src/state/context.ts
+++ src/state/context.ts
@@ -1,7 +1,14 @@
 import { createContext } from 'react';
-import defaultState, { IState, MinuteContextType } from './state';
+import defaultState, {
+  IState,
+  MinuteContextType,
+  OptionContextType,
+} from './state';
 
 export const StateContext = createContext<IState>(defaultState);
 export const TimeContext = createContext<MinuteContextType | undefined>(
+  undefined
+);
+export const OptionContext = createContext<OptionContextType | undefined>(
   undefined
 );
