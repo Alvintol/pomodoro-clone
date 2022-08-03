8118ad563b1d03130427afb3666f559503c86fd4
Alvin <alvin.tol@hotmail.com>
2022-08-02 20:42:50 -0600

    adds conditional rendering for play/pause button

--- dist/components/Controls.jsx
+++ dist/components/Controls.jsx
@@ -1,9 +1,12 @@
+import { useContext } from 'react';
+import { StateContext } from '../state/context';
 import Control from './Control';
 const Controls = () => {
+    const state = useContext(StateContext);
+    const { play } = state;
     const controlsClass = 'flex flex-row justify-center';
     return (<div data-testid='controls' id='controls' className={controlsClass}>
-      <Control id='START'/>
-      <Control id='PAUSE'/>
+      {play ? <Control id='PAUSE'/> : <Control id='START'/>}
       <Control id='RESET'/>
     </div>);
 };
--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,17 +1,16 @@
+import { useContext } from 'react';
+import { StateContext } from '../state/context';
 import Control from './Control';
 
 const Controls = () => {
+  const state = useContext(StateContext);
+  const { play } = state;
 
-const controlsClass: string = 'flex flex-row justify-center';
+  const controlsClass: string = 'flex flex-row justify-center';
 
   return (
-    <div
-      data-testid='controls'
-      id='controls'
-      className={controlsClass}
-    >
-      <Control id='START' />
-      <Control id='PAUSE' />
+    <div data-testid='controls' id='controls' className={controlsClass}>
+      {play ? <Control id='PAUSE' /> : <Control id='START' />}
       <Control id='RESET' />
     </div>
   );
