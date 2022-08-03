6db54785ea623b9e837a525b6d6e5f0a9b867092
Alvin <alvin.tol@hotmail.com>
2022-08-02 19:29:13 -0600

    imports and adds changeOption function to session button]

--- dist/components/Session.jsx
+++ dist/components/Session.jsx
@@ -1,10 +1,14 @@
 import { useContext } from 'react';
 import { isToggled } from '../helpers/helperFunctions';
-import { StateContext } from '../state/context';
+import { OptionContext, StateContext } from '../state/context';
 const Session = () => {
     const state = useContext(StateContext);
+    const select = useContext(OptionContext);
     const { option } = state;
     const sessionClass = isToggled('session', option) + ' bg-pink w-full md:w-1/3 ';
-    return (<div data-testid='session' id='session' className={sessionClass}>Session</div>);
+    const handleClick = () => {
+        select?.changeOption('session');
+    };
+    return (<div data-testid='session' id='session' className={sessionClass} onClick={handleClick}>Session</div>);
 };
 export default Session;
--- src/components/Session.tsx
+++ src/components/Session.tsx
@@ -1,16 +1,26 @@
 import { useContext } from 'react';
 import { isToggled } from '../helpers/helperFunctions';
-import { StateContext } from '../state/context';
+import { OptionContext, StateContext } from '../state/context';
 
 const Session = () => {
 
-  const state = useContext(StateContext)
+  const state = useContext(StateContext);
+  const select = useContext(OptionContext);
   const {option} = state;
 
   const sessionClass: string = isToggled('session', option) + ' bg-pink w-full md:w-1/3 ';
 
+  const handleClick = (): void => {
+    select?.changeOption('session');
+  };
+
   return (
-    <div data-testid='session' id='session' className={sessionClass}>Session</div>
+    <div 
+    data-testid='session' 
+    id='session' 
+    className={sessionClass}
+    onClick={handleClick}
+    >Session</div>
   )
 };
 
